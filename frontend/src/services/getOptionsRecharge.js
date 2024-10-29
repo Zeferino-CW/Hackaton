a95bfd44944ba341f4acc91d761f9e4ee959a116
thmanhaes <thiago.manhaes@cloudwalk.io>
2024-10-28 23:54:57 -0300

    criacao da home do app. faltando CSS.

---
+++ frontend/src/services/getOptionsRecharge.js
@@ -0,0 +1,29 @@
+import InfPayWhiteGrd from "../images/InfinitePayWhiteGround.png";
+import ArrowRightIcon from "../images/icon-arrow-right.png";
+import InfPayTripleWhiteGrd from "../images/tripleInfinite.png";
+
+const recharges = [
+    {
+        image: InfPayWhiteGrd,
+        title: "Telefonia e TV",
+        text: "Recargas de Celular, Telefone Fixo e TV",
+        buttonText: "Vender",
+        arrowRightIcon: ArrowRightIcon
+    },
+    {
+        image: InfPayTripleWhiteGrd,
+        title: "Games e Entretenimento",
+        text: "Games, Apps e Etc",
+        buttonText: "Vender",
+        arrowRightIcon: ArrowRightIcon
+    },
+    {
+        image: InfPayTripleWhiteGrd,
+        title: "Vouchers e Vale Compras",
+        text: "Vouchers, Vale Compras Etc",
+        buttonText: "Vender",
+        arrowRightIcon: ArrowRightIcon
+    }
+]
+
+export default recharges;
