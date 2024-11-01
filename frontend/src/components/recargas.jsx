dc4c8643568295789937de27c6eb4c3a20fd9b0c
thmanhaes <thiago.manhaes@cloudwalk.io>
2024-10-31 22:25:07 -0300

    página de recarga de telefonia coletando as informações e transformando num body para enviar para request. Falta fazer o modal da requisição e enviar para o endpoint

--- frontend/src/components/recargas.jsx
+++ frontend/src/components/recargas.jsx
@@ -1,8 +1,18 @@
 import Image from "next/image";
 import recharges from "../services/getOptionsRecharge"
+import { useRouter } from 'next/router';
 import "../style/recargas.css";
 
 export default function Recarga () {
+  const router = useRouter();
+
+  const goToRecharges = (ind) => {
+    if (ind == 0) {
+      router.push('/telefonia');
+    }
+    
+  }
+
   return (
     <div classe="divMae-comp-recarga">
         {
@@ -13,7 +23,7 @@ export default function Recarga () {
               <p>{text}</p>
               <button
                 type="button"
-                
+                onClick={() => { goToRecharges(ind) } }
               >
                 {buttonText}
                 <Image src={arrowRightIcon} alt="Ícone de seta" className="icon-arrow"  />
--- frontend/src/pages/telefonia.jsx
+++ frontend/src/pages/telefonia.jsx
@@ -1,31 +1,78 @@
 import Image from "next/image";
 import InfPaySimpleSymbol from "../images/InfinitePayWhiteGround.png"
-import {rechargeType, operadoras, rechargeValues} from "../services/getTelefonia";
+// import {rechargeType, operadoras, rechargeValues} from "../services/getTelefonia";
+import { useRouter } from 'next/router';
+import { useState, useEffect } from 'react';
 import telefonia from "../services/getTelefonia";
 
 export default function Telefonia () {
+  const router = useRouter();
+  const [operadora, setOperadora] = useState('');
+  const [recarga, setRecarga] = useState('');
+  const [telefone, setTelefone] = useState('');
+  const [isBtnRechargeBlock, setIsBtnRechargeBlock] = useState(true);
+
+  const controlGeneralState = ({ target }) => {
+    const { value, name } = target;
+
+    if (name === 'input-operadora') setOperadora(value);
+    if (name === 'input-valor-recarga') setRecarga(value);
+    if (name === 'telefone') setTelefone(value);
+  }
+
+  const validPhoneNumber = () => {
+    const regex = /^\(?\d{2}\)?\s?\d{5}-?\d{4}$/;
+    return regex.test(telefone);    
+  }
+
+  const validateButton = () => {
+    const numberPhoneIsOK = validPhoneNumber()
+    if (numberPhoneIsOK && operadora.length > 1 && recarga.length > 1) {
+      setIsBtnRechargeBlock(false)
+    } else {
+      setIsBtnRechargeBlock(true)
+    }
+  }
+
+  const makeRequest = () => {
+    const { operadorasId } = telefonia;
+    const bodyToRequest = {
+      product_id: operadorasId[operadora].toString(),
+      area_code: telefone.slice(0,2).toString(),
+      cell_phone_number: telefone.slice(2).toString(),
+      amount: recarga
+    }
+  }
+  useEffect(() => {
+    validateButton();
+  }, [operadora, recarga, telefone])
+
   return (
     <div id="telefonia">
       <div id="card">
         <Image src={InfPaySimpleSymbol}/>
         <h2>Telefonia</h2>
-        <p>Aqui você pode vender recargas de celular pré-pago, recargas de telefone fixo e tvs pré pagas.</p>
+        <p>Aqui você pode vender recargas de celular pré-pago e recargas de telefone fixo.</p>
       </div>
 
       <div id="form-recarga">
         <h3>Recarga escolhida</h3>
         <form action="">
-          <label htmlFor="select-recarga-tipo">Tipos de recarga disponíveis</label>
+          {/* <label htmlFor="select-recarga-tipo">Tipos de recarga disponíveis</label>
           <select name="input-recarga-tipo" id="select-recarga-tipo">
             {
               telefonia.rechargeType.map((type, ind) => (
                 <option value={type} key={ind}>{type}</option>
               ))
             }
-          </select>
+          </select> */}
 
           <label htmlFor="select-operadoras">Operadoras disponíveis</label>
-          <select name="input-operadora" id="select-operadoras">
+          <select 
+            name="input-operadora"
+            id="select-operadoras"
+            onChange={controlGeneralState}
+          >
             {
               telefonia.operadoras.map((type, ind) => (
                 <option value={type} key={ind}>{type}</option>
@@ -33,8 +80,12 @@ export default function Telefonia () {
             }
           </select>
 
-          <label htmlFor="select-operadoras">Operadoras disponíveis</label>
-          <select name="input-operadora" id="select-operadoras">
+          <label htmlFor="select-valor-recarga">Recargas disponíveis</label>
+          <select
+            name="input-valor-recarga" 
+            id="select-valor-recarga"
+            onChange={controlGeneralState}
+          >
             {
               telefonia.rechargeValues.map((type, ind) => (
                 <option value={type} key={ind}>{type}</option>
@@ -42,18 +93,30 @@ export default function Telefonia () {
             }
           </select>
 
-          <label for="telefone">Número do celular/telefone:</label>
-          <input type="tel" id="telefone" name="telefone" placeholder="(XX) XXXXX-XXXX" pattern="\(\d{2}\) \d{4,5}-\d{4}" required />
+          <label htmlFor="telefone">Número do celular/telefone:</label>
+            <input 
+              type="tel"
+              id="telefone"
+              name="telefone"
+              placeholder="(XX) XXXXX-XXXX"
+              pattern="\(\d{2}\) \d{4,5}-\d{4}"
+              onChange={controlGeneralState}
+              maxLength={11}
+              required
+            />
 
           <div id="button">
             <button
               type="button"
+              onClick={ () => router.push('/')}
             >
               Voltar ao início
             </button>
 
             <button
               type="button"
+              disabled={isBtnRechargeBlock}
+              onClick={ makeRequest }
             >
               Fazer recarga
             </button>
