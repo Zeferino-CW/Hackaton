82d75984f9b59ae9da25916dac94fca3d2195c0a
VZeferino <vitor.queiroz@sou.inteli.edu.br>
2024-11-04 22:35:31 -0300

    feat: works to do

--- frontend/src/pages/payment.jsx
+++ frontend/src/pages/payment.jsx
@@ -9,12 +9,14 @@ export default function Payment () {
         <Image src={iconInfPay} alt="ícone InfinitePay"/>
         <h3>Como quer receber?</h3>
         <p>Escolha o meio de pagamento.</p>
+        {/* Vamos fazer o fluxo seja por débito ou por crédito, se for cŕedito deve-se definir o número de parcelas. */}
       </section>
 
       <section id="section-payment">
         <div id="valor">
           <h3>Meio de pagamento</h3>
-          <p>*estado global do valor vai entrar aqui*</p>
+          <p>*estado global do valor vai entrar aqui*</p> 
+          {/* Colocar o valor do pagamento aqui */}
         </div>
 
         <div id="div-payment-type">
@@ -36,6 +38,9 @@ export default function Payment () {
             Voltar ao início
           </button>
 
+{/* Quando clicar nesse botão de recarga ele deve ser dinamico com a url para abrir o tap baseado com a forma de pagamento escolhida
+link para a documentação aqui: https://www.infinitepay.io/desenvolvedores 
+Exemplo: infinitepaydash://infinitetap-app? amount=100&payment_method=credit&installments=1&order_id=3262&result_url=mypocapp%3A%2F%2Fexample%2Ftap_result&app_client_referrer=POCApp&af_force_deeplink=true */}
           <button
             type="button"
           >
